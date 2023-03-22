fb533eda84c1b4123555a44218e6369f67223474
Bernard <bernard.koech@student.moringaschool.com>
2023-03-22 09:58:23 +0300

    commit tree 497d3b0ed4f614fde52d3deebafbc638a772003c

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@ Cheese.create!(name: 'Cheddar', price: 3, is_best_seller: true)
 Cheese.create!(name: 'Pepper Jack', price: 4, is_best_seller: true)
 Cheese.create!(name: 'Limburger', price: 8, is_best_seller: false)
 
-puts '✅ Done seedingg'
+puts '✅ Done seeding'
